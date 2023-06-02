1e704910f74c498579b04e5ce10d08702db46c00
Erol Cherim <erol.cherim@gmail.com>
2023-06-02 12:55:58 -0400

    [API] Fixed get listings added by user not returning listings in reverse chronological order

--- src/main/java/com/unibuc/auclicenta/service/ListingService.java
+++ src/main/java/com/unibuc/auclicenta/service/ListingService.java
@@ -55,8 +55,8 @@ public class ListingService {
 
     public SearchResponse getListingsForUser(int page, int pageSize) {
         String userId = userService.getUserIdByEmail(SecurityContextHolder.getContext().getAuthentication().getName());
-        List<Listing> listings = listingRepository.findByUserIdAndIsActive(userId, 2, PageRequest.of(page, pageSize)).toList(); //TODO modify to 1
-        Long noListings = listingRepository.findByUserIdAndIsActive(userId, 2, PageRequest.of(page, pageSize)).getTotalElements();
+        List<Listing> listings = listingRepository.findByUserIdAndIsActive(userId, 2, PageRequest.of(page, pageSize).withSort(Sort.Direction.DESC, "createdDate")).toList(); //TODO modify to 1
+        Long noListings = listingRepository.findByUserIdAndIsActive(userId, 2, PageRequest.of(page, pageSize).withSort(Sort.Direction.DESC, "createdDate")).getTotalElements();
         return SearchResponse.builder().listings(listings).noResults(noListings).build();
     }
 
